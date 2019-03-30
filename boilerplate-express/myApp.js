29aad508ac472c6a2fe18a76d43fdf08a568715c
Justin Peck <jdpeck90@gmail.com>
2019-03-30 00:47:08 -0400

    2nd challenge

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -9,7 +9,9 @@ var app = express();
 console.log("Hello World");
 
 /** 2) A first working Express Server */
-
+app.get('/', function(req,res){
+    res.send('Response String')
+})
 /** 3) Serve an HTML file */
 
 /** 4) Serve static assets  */
